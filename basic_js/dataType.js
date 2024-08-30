b3cad6d1140e881b93c2c45ffe44225b158c9f15
Sadanand sharma <sharmasadanand707@gmail.com>
2024-08-30 19:47:54 +0000

    datatype

---
+++ basic_js/dataType.js
@@ -0,0 +1,31 @@
+"use strict";//treat all Js code as newer version
+
+//alert(3+3); // i know this code are popup in the browser but here not allow becouse this code are run in the node js.
+
+console.log(3
+                 //this is not better for code readability
+    +3);
+console.log("rohit sharma")
+
+let name = "rohit";
+let age=20;
+let isLoggedIn=false;
+
+//number => 2 to power 53;
+//bigint
+//string
+//boolean => true/false
+//null => standalone value
+//undefined 
+//symbol => unique
+
+
+
+//boject
+
+console.log(typeof"hitesh");//string
+console.log(typeof age);//number
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
