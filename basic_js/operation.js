b16d2b4aeeed67481215a3c628727ff5cf29b6c7
Sadanand sharma <sharmasadanand707@gmail.com>
2024-09-03 09:57:09 +0000

    conversionOperation

---
+++ basic_js/operation.js
@@ -0,0 +1,18 @@
+console.log("hello world");
+
+let score=46;  //data type in this variable is number
+
+console.log(typeof score);  //number
+console.log(typeof(score)); //number
+
+//but 
+
+// let the variable is string then those variable how to conver in number.
+
+
+let scores="33";
+console.log(typeof(scores));// string
+let valueInNumber=Number(scores);//the string value are changed into number
+                                 //how it is possible becouse there are using a Number function.
+
+console.log(typeof(valueInNumber));//number
