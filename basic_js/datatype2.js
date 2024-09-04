a8713825a38580ce2d885add246c276027508f61
Sadanand sharma <sharmasadanand707@gmail.com>
2024-09-04 12:40:23 +0000

    datatype

--- basic_js/datatype2.js
+++ basic_js/datatype2.js
@@ -10,7 +10,7 @@ there are 7 type:
     BigInt
 
 */
-
+ 
 const score=100;
 console.log(typeof score);//number
 const scoreValue=100.3;
@@ -20,7 +20,31 @@ const temp=null;
 let userEmail=undefined;
 const id=Symbol('123');
 const anotherId=Symbol('123');
-
 console.log(id===anotherId);//not equals to id and anotherId
 
 const bignumber=4547454222222656655n;
+
+
+//non primitive data type(reference data type)
+/********************************************
+
+ Any value Directly allocated the reference in a memory
+
+
+ note: javaScript object and browsers web event are so improtant 
+
+
+ -array
+ -Object 
+ -Function
+
+ Qs:javaScript are dynamically type or statically type language?????
+ Ans:Dynamically 
+
+Statically type:  c, c++, Java
+Dynamically type: javaScript, python
+
+
+
+
+
