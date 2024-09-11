04a1a14332154dbd5d90dbb282bb61d7934286b3
Sadanand sharma <sharmasadanand707@gmail.com>
2024-09-11 02:04:09 +0000

    full concept of string in this file

---
+++ String_js/string.js
@@ -0,0 +1,17 @@
+//normal using string like as:
+
+const name="sadanad sharma";
+const repocount=50;
+
+//console.log(name+repocount);//using this kinds of formet are old patterns
+
+console.log(`hello my name is ${name} by occupation i am student and my repo count is ${repocount}`);
+// The mordern way to using a strings in the form of backtake
+// (`- - - - -`${variable name}`- - - - - `)//this is the best method to write a strings in the javascript
+
+
+
+//string declared 
+
+const gameName=new String('rohit');
+console.log(gameName);
